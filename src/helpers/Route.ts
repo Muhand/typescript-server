888f6109d960ea019800e7c4d07dfccb5d693216
Muhand Jumah <muhandjumah@gmail.com>
2018-05-26 14:19:17 -0400

    Changed the base class

--- src/helpers/Route.ts
+++ src/helpers/Route.ts
@@ -1,6 +1,24 @@
 import * as express from 'express'
+import {Router, Request, Response, NextFunction} from 'express';
 
-export class Route {
-    routehandler: (req: express.Request, res: express.Response, next: Function) => {}
-    url: string
+export abstract class Route {
+    private _router: Router    
+    private _url: string
+    abstract init(): void
+    abstract setupRoutes(): void
+
+    //#region properties setters/getters
+    get router(): Router {
+        return this._router
+    }
+    set router(router:Router) {
+        this._router = router
+    }
+    get url(): string {
+        return this._url
+    }
+    set url(url:string) {
+        this._url = url
+    }
+    //#endregion
 }
