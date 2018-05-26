0c145dd1a34f74eb9918c65cde376c754a759aca
Muhand Jumah <muhandjumah@gmail.com>
2018-05-26 14:35:18 -0400

    No more need for the url property

--- src/helpers/Route.ts
+++ src/helpers/Route.ts
@@ -3,7 +3,6 @@ import {Router, Request, Response, NextFunction} from 'express';
 
 export abstract class Route {
     private _router: Router    
-    private _url: string
     abstract init(): void
     abstract setupRoutes(): void
 
@@ -13,12 +12,6 @@ export abstract class Route {
     }
     set router(router:Router) {
         this._router = router
-    }
-    get url(): string {
-        return this._url
-    }
-    set url(url:string) {
-        this._url = url
     }
     //#endregion
 }
